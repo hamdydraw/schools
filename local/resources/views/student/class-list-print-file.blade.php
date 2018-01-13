02fd2929c14ead89225421149aafeb7d424c0d67
Montaser Elsawy <montaserelsawy@gmail.com>
2018-01-14 00:04:03 +0300

    print direction rtl

--- local/resources/views/student/class-list-print-file.blade.php
+++ local/resources/views/student/class-list-print-file.blade.php
@@ -1,7 +1,7 @@
-
+<div style="float: right;direction:rtl;">
 <h1><b>{{$title}}</b></h1><br/>
 <div class="row vertical-scroll">
-  
+
     <table style="border-collapse: collapse;">
 
     <thead>
@@ -12,15 +12,15 @@
         @for($i=1; $i<=$extracols;$i++)
         <th style="border:1px solid #000;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
         @endfor
-       
-        
+
+
     </thead>
     <tbody>
     <?php $sno =1;?>
      @foreach($records as $record)
-    
+
     <tr>
-        
+
         <td style="border:1px solid #000;">{{$sno++}}</td>
         <td style="border:1px solid #000;">{{$record['name']}}</td>
         <td style="border:1px solid #000;">{{$record['roll_no']}}</td>
@@ -28,10 +28,11 @@
          @for($i=1; $i<=$extracols;$i++)
         <th style="border:1px solid #000;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
         @endfor
-        
-       
-    </tr> 
+
+
+    </tr>
     @endforeach
     </tbody>
     </table>
+</div>
 </div>
