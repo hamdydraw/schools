f96f8e0a25f90fb9616ae2025f258cfae113d3e7
mahmoudafifi123 <pro.afifi100@gmail.com>
2018-04-04 11:32:23 +0200

    supervisor completeness (not to revision this section)

--- local/app/Http/Controllers/DuesController.php
+++ local/app/Http/Controllers/DuesController.php
@@ -45,7 +45,7 @@ class DuesController extends Controller
 
     public function store(Request $request)
     {
-        $toBeDeleted = DB::select('Delete from academics_dues_pivot where academic_id = ?', [$request->academic_year]);
+        $toBeDeleted = DB::delete('Delete from academics_dues_pivot where academic_id = ?', [$request->academic_year]);
         if ($request->academic_year == "select" or $request->academic_dues == "select" or $request->due_value == "select" or $request->due_type == "select") {
             flash(getPhrase('error'), getPhrase("you_should_fill_all_fields"), 'error');
             return redirect()->back();
