eb47ba94aba1663f394566c1656ef869626b3ae5
MostafaAboelnaga <moustafa.aboelnaga@students.mans.edu.eg>
2018-04-18 23:42:18 +0200

    Commit Students-Reports

--- local/resources/views/student/cousrse-completd-list-print-file.blade.php
+++ local/resources/views/student/cousrse-completd-list-print-file.blade.php
@@ -1,34 +1,116 @@
-<div style="float: right;direction:rtl;">
-    <h1><b>{{$title}}</b></h1><br/>
-    <div class="row vertical-scroll">
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-        <table style="border-collapse: collapse;">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="assets/images/favicon.ico">
+    <title>{{$title}}</title>
 
-            <thead>
-            <th style="border:1px solid #000;">{{getPhrase('sn')}}</th>
-            <th style="border:1px solid #000;">{{getPhrase('name')}}</th>
-            <th style="border:1px solid #000;">{{getPhrase('roll_no')}}</th>
-            <th style="border:1px solid #000;">{{getPhrase('course')}}</th>
-            <th style="border:1px solid #000;">{{getPhrase('status')}}</th>
+    <link rel="stylesheet" href="{{CSS}}Report/font-icons/entypo/css/entypo.css">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
+    <link rel="stylesheet" href="{{CSS}}Report/bootstrap.css">
+    <link rel="stylesheet" href="{{CSS}}Report/neon-core.css">
+    <link rel="stylesheet" href="{{CSS}}Report/neon-theme.css">
+    <link rel="stylesheet" href="{{CSS}}Report/neon-rtl.css">
 
 
-            </thead>
-            <tbody>
-            <?php $sno = 1;?>
-            @foreach($records as $record)
+</head>
+<body class="page-body">
 
+<div class="page-container">
+
+    <div class="main-content">
+
+        <br class="hidden-print" />
+
+        <div class="invoice">
+
+            <div class="row">
+
+                <div class="col-sm-4 invoice-right" style="text-align: right">
+                    <h3>{{getSetting('site_title','site_settings')}}</h3>
+                </div>
+
+                <div class="col-sm-4 invoice-left pull-left" style="text-align: left">
+                    <a href="#">
+                        <img src="{{IMAGE_PATH_SETTINGS.getSetting('site_logo', 'site_settings')}}" width="195" style="margin-left: 80px;" alt="" />
+                    </a>
+                </div>
+
+                <div class="col-sm-4 pull-right" style="text-align: left">
+
+                    <h3>{{$title}}</h3>
+                </div>
+
+
+
+
+
+            </div>
+
+
+            <hr class="margin" />
+
+
+            <div class="margin"></div>
+
+            <table class="table table-bordered">
+                <thead>
                 <tr>
-
-                    <td style="border:1px solid #000;">{{$sno++}}</td>
-                    <td style="border:1px solid #000;">{{$record['name']}}</td>
-                    <td style="border:1px solid #000;">{{$record['roll_no']}}</td>
-                    <td style="border:1px solid #000;">{{$record['course_title']}}</td>
-                    <td style="border:1px solid #000;">{{getPhrase('completed')}}</td>
-
-
+                    <th style="border:1px solid #000;">{{getPhrase('sn')}}</th>
+                    <th style="border:1px solid #000;">{{getPhrase('name')}}</th>
+                    <th style="border:1px solid #000;">{{getPhrase('roll_no')}}</th>
+                    <th style="border:1px solid #000;">{{getPhrase('course')}}</th>
+                    <th style="border:1px solid #000;">{{getPhrase('status')}}</th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+                <?php $sno =1;?>
+                @foreach($records as $record)
+                    <tr>
+
+                        <td style="border:1px solid #000;">{{$sno++}}</td>
+                        <td style="border:1px solid #000;">{{$record['name']}}</td>
+                        <td style="border:1px solid #000;">{{$record['roll_no']}}</td>
+                        <td style="border:1px solid #000;">{{$record['course_title']}}</td>
+                        <td style="border:1px solid #000;">{{getPhrase('completed')}}</td>
+
+
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+
+            <div class="margin"></div>
+
+            <div class="row">
+
+                <div class="col-sm-12">
+
+                    <div class="invoice-right">
+                        <br />
+
+                        <a href="javascript:window.print();" class="btn btn-primary btn-icon icon-left hidden-print">
+                            طباعة او حفظ بي دي أف
+                            <i class="entypo-doc-text"></i>
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+        <!-- Footer -->
+
     </div>
+
 </div>
+
+
+</body>
+</html>
+
