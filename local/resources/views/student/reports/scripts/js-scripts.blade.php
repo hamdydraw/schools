ad0e3eda31e74fb1a2c6d3f11a9c4d7c94b4097a
MostafaAboelnaga <moustafa.aboelnaga@students.mans.edu.eg>
2018-04-20 22:07:34 +0200

    Students_Exam_Results_admin

--- local/resources/views/student/reports/scripts/js-scripts.blade.php
+++ local/resources/views/student/reports/scripts/js-scripts.blade.php
@@ -2,94 +2,110 @@
 @include('common.angular-factory')
 
 <script>
- app.controller('TabController', function ($scope, $http, httpPreConfig,$location)
-  {
-     @include('common.js-script-year-selection',array('user_slug'=>$user->slug))
-
-
-      $scope.exam_categories = [];
-      $scope.exam_list       = []; 
-      $scope.show_div = false;
-
-      $scope.doCall     = function () {
-      $scope.year_selected   = false;
-      $scope.resetResult();
- 
-      academic_id          = '{{prepareStudentSessionRecord($user_slug)->student->academic_id}}';
-      parent_course_id     = '{{prepareStudentSessionRecord($user_slug)->student->course_parent_id}}';
-      course_id            = '{{prepareStudentSessionRecord($user_slug)->student->course_id}}';
-      year                 = '{{prepareStudentSessionRecord($user_slug)->student->current_year}}';
-      semister                 = '{{prepareStudentSessionRecord($user_slug)->student->current_semister}}';
-        user_id          = '{{$user->id}}';
-        route   = '{{URL_STUDENT_RESULTS_GET_EXAM_CATEGORIES}}';  
-        data    = {   _method: 'post', 
-                  '_token':httpPreConfig.getToken(), 
-                  'academic_id': academic_id, 
-                  'parent_course_id': parent_course_id,
-                  'course_id': course_id,
-                  'year': year,
-                  'semister': semister,
-                  'user_id': user_id,
-               };
-               
-         httpPreConfig.webServiceCallPost(route, data).then(function(result){
-          result = result.data;
-        categories = [];
-        angular.forEach(result, function(value, key) {
-            categories.push(value);
-          })
-
-        $scope.exam_categories = categories;
-        if($scope.exam_categories.length >0 )
-        $scope.show_div = true;
-        });
-    }
-    $scope.setTab   = function(category_id){
-      
-    $scope.tab      = category_id;
-
-      academic_id      = $scope.selected_academic_id;
-      parent_course_id = $scope.selected_course_parent_id;
-      course_id        = $scope.selected_course_id;
-      year             = $scope.selected_year;
-      semister         = $scope.selected_semister;
-      user_id          = '{{$user->id}}';
-      route            = '{{URL_STUDENT_RESULTS_GET_EXAMS}}';  
-        data           = {   _method: 'post', 
-                            '_token':httpPreConfig.getToken(), 
-                            'academic_id': academic_id, 
-                            'parent_course_id': parent_course_id,
-                             'course_id': course_id,
-                             'year': year,
-                             'semister': semister,
-                             'category_id': category_id,
-                             'user_id': user_id,
-                         };
-               
-         httpPreConfig.webServiceCallPost(route, data).then(function(result){
-         result = result.data;
-         examList = [];
-        angular.forEach(result, function(value, key) {
-            examList.push(value);
-          })
-
-        $scope.exam_list = examList;
-        
-     
-        });
-      
-    };
-
-    $scope.isSet         = function(tabNum){
-      return $scope.tab === tabNum;
-    };
-    
-    $scope.resetResult = function()
+    app.controller('TabController', function ($scope, $http, httpPreConfig,$location)
     {
-       $scope.show_div = false;
-       $scope.exam_list =[];
-    }
-});
- 
-  
+        @include('common.js-script-year-selection',array('user_slug'=>$user->slug))
+
+
+            $scope.exam_categories = [];
+        $scope.exam_list       = [];
+        $scope.show_div = false;
+
+        $scope.doCall     = function () {
+            $scope.year_selected   = false;
+            $scope.resetResult();
+
+            @if(isset($admin))
+            {
+                $scope.show_div = false;
+                academic_id      = $scope.selected_academic_id;
+                parent_course_id = $scope.selected_course_parent_id;
+                course_id        = $scope.selected_course_id;
+                year             = $scope.selected_year;
+                semister         = $scope.selected_semister;
+                user_id          = '{{ $user->id }}';
+                route            = '{{ URL_STUDENT_RESULTS_GET_EXAMS }}';
+            }
+            @else
+            {
+                academic_id          = '{{prepareStudentSessionRecord($user_slug)->student->academic_id}}';
+                parent_course_id     = '{{prepareStudentSessionRecord($user_slug)->student->course_parent_id}}';
+                course_id            = '{{prepareStudentSessionRecord($user_slug)->student->course_id}}';
+                year                 = '{{prepareStudentSessionRecord($user_slug)->student->current_year}}';
+                semister                 = '{{prepareStudentSessionRecord($user_slug)->student->current_semister}}';
+                user_id          = '{{$user->id}}';
+            }
+            @endif
+
+            route   = '{{URL_STUDENT_RESULTS_GET_EXAM_CATEGORIES}}';
+            data    = {   _method: 'post',
+                '_token':httpPreConfig.getToken(),
+                'academic_id': academic_id,
+                'parent_course_id': parent_course_id,
+                'course_id': course_id,
+                'year': year,
+                'semister': semister,
+                'user_id': user_id,
+            };
+
+            httpPreConfig.webServiceCallPost(route, data).then(function(result){
+                result = result.data;
+                categories = [];
+                angular.forEach(result, function(value, key) {
+                    categories.push(value);
+                })
+
+                $scope.exam_categories = categories;
+                if($scope.exam_categories.length >0 )
+                    $scope.show_div = true;
+            });
+        }
+        $scope.setTab   = function(category_id){
+
+            $scope.tab      = category_id;
+
+            academic_id      = $scope.selected_academic_id;
+            parent_course_id = $scope.selected_course_parent_id;
+            course_id        = $scope.selected_course_id;
+            year             = $scope.selected_year;
+            semister         = $scope.selected_semister;
+            user_id          = '{{$user->id}}';
+            route            = '{{URL_STUDENT_RESULTS_GET_EXAMS}}';
+            data           = {   _method: 'post',
+                '_token':httpPreConfig.getToken(),
+                'academic_id': academic_id,
+                'parent_course_id': parent_course_id,
+                'course_id': course_id,
+                'year': year,
+                'semister': semister,
+                'category_id': category_id,
+                'user_id': user_id,
+            };
+
+            httpPreConfig.webServiceCallPost(route, data).then(function(result){
+                result = result.data;
+                examList = [];
+                angular.forEach(result, function(value, key) {
+                    examList.push(value);
+                })
+
+                $scope.exam_list = examList;
+
+
+            });
+
+        };
+
+        $scope.isSet         = function(tabNum){
+            return $scope.tab === tabNum;
+        };
+
+        $scope.resetResult = function()
+        {
+            $scope.show_div = false;
+            $scope.exam_list =[];
+        }
+    });
+
+
 </script>
