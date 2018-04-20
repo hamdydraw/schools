d4410198f226490064ff2988081762e4068510d3
MostafaAboelnaga <moustafa.aboelnaga@students.mans.edu.eg>
2018-04-20 10:10:55 +0200

    FixLocaliztion_StudentReports

--- local/app/Language.php
+++ local/app/Language.php
@@ -42,6 +42,7 @@ class Language extends Model
    public function addPhrase($phrase, $default_language = 0)
    {
 
+
    		if($default_language==0)
    			$default_language_id = Language::getDefaultLanguage();
 
