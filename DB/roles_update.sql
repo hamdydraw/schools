e8c778f3ac6d50f97e28a3fe1aafcf47bb2aa16d
Ahmed Awd <theimaginaryking@gmail.com>
2018-11-09 14:05:15 +0200

    roles table sql edit

---
+++ DB/roles_update.sql
@@ -0,0 +1,2 @@
+INSERT INTO `roles` (`id`, `name`, `display_name`, `description`, `created_at`, `updated_at`, `updated_by_ip`, `created_by_ip`, `created_by_user`, `updated_by_user`, `record_status`, `table_name`, `branch_id`) VALUES
+(11, 'student_guide', 'Student Guide', 'Guide for Students', NULL, NULL, NULL, NULL, NULL, NULL, 1, 'roles', 1);
