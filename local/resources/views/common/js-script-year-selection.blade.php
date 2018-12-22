6920240bf4e0f611da18a042d7db45048ceb4fff
Sniper <prog.a.bahnasy@gmail.com>
2018-12-22 02:18:58 +0200

    activated semester selection

--- local/resources/views/common/js-script-year-selection.blade.php
+++ local/resources/views/common/js-script-year-selection.blade.php
@@ -169,7 +169,10 @@ if($location.absUrl().split('/')[$location.absUrl().split('/').length-1] === "tr
 || $location.absUrl().split('/')[$location.absUrl().split('/').length-1] === "id-cards"
 || $location.absUrl().split('/')[$location.absUrl().split('/').length-2]+$location.absUrl().split('/')[$location.absUrl().split('/').length-1] === "usersimport"
 || $location.absUrl().split('/')[$location.absUrl().split('/').length-3]+$location.absUrl().split('/')[$location.absUrl().split('/').length-2] === "profileedit"
-|| $location.absUrl().split('/')[$location.absUrl().split('/').length-4]+$location.absUrl().split('/')[$location.absUrl().split('/').length-3] === "studentattendance"
+{{-- 
+    hashed to show semesters on student attendance report
+    || $location.absUrl().split('/')[$location.absUrl().split('/').length-4]+$location.absUrl().split('/')[$location.absUrl().split('/').length-3] === "studentattendance" 
+    --}}
 || $location.absUrl().split('/')[$location.absUrl().split('/').length-1] === "record-student"
 )
 {
